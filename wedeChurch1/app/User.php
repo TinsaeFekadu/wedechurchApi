fa5d318aa521c418e6fec393443690b861f134c1
Tinsu <tinsaefekadu@gmail.com>
2017-11-24 07:07:09 -0300

    add existing file

--- wedeChurch1/app/User.php
+++ wedeChurch1/app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+   // use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+       'name', 'email', 'password'
     ];
 
     /**
@@ -23,7 +23,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
+  protected $hidden = [
         'password', 'remember_token',
     ];
 }
--- wedeChurch1/routes/api.php
+++ wedeChurch1/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Church;
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +13,58 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/*
 
+Route::get('/churchs', function()
+{
+   return Church::all();
+});
+
+Route::get('/churchs/{id}',function($id)
+{
+    return Church::find($id);
+});
+
+Route::post('/churchs',function(Request $request)
+{
+  //return Church::create($request->all);
+
+    $church=Church::create($request->all());
+    return response()->json($church, 201);
+});
+
+Route::post('churchs/{id}/update',function(Request $request,$id)
+{
+    $church = Church::findOrFail($id);
+    $church->update($request->all());
+
+    return Church::find($id);
+});
+
+Route::post('churchs/{id}/delete',function($id)
+{
+    Church::find($id)->delete();
+
+        return ('Deleted!!!');
+});
+*/
+
+Route::get('churchs', 'ChurchsController@index');
+Route::get('churchs/{id}', 'ChurchsController@show');
+Route::post('churchs', 'ChurchsController@store');
+Route::post('churchs/{id}/update', 'ChurchsController@update');
+Route::post('churchs/{id}/delete', 'ChurchsController@delete');
+
+
+Route::get('users', 'UsersController@index');
+Route::get('users/{id}', 'UsersController@show');
+Route::post('users', 'UsersController@store');
+Route::post('users/{id}/update', 'UsersController@update');
+Route::post('users/{id}/delete', 'UsersController@delete');
+
+
+
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
